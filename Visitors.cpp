439b82061d0e4ef03de2c26a74e3c968ee706e2e
Stalkie666 <hamatlama@gmail.com>
2022-04-12 13:28:03 +0200

    First Commit

---
+++ Visitors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+//#1
+//loading data from .txt file
+
+//nezapomenout osetrit pripadne chyby na vstupu,
+//pozor na chybejici a prebivajici znaky, chybne znaky a dalsi kulisarny
+
+
+//#2
+//print hall occupancy
+
+
+//#3
+//profit from occuied seats
+
+
+
+int main(){
+	cout << "Hello world" << endl;
+	return 0;
+}
